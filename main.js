4e2cb7384116dc807ad1a6daa235252ee1d02ac9
AndyLampert <lamperta@gmail.com>
2014-04-18 14:23:58 -0600

    changed function into a pure function

--- main.js
+++ main.js
@@ -3,25 +3,31 @@ var pluralize = function(word,number){
 	return (number === 1)? word : word + s;
 }
 
-// The first step in functional programming is to write as much of your code in pure functions as possible. After that, you can make your functions more 'functional' still by not letting them rely on global variables; instead pass all needed variables as arguments. This makes your function even more modular, just input/output.
-
 // Rewrite the following code to be more functional:
-var victim = {};
-
+// so that it effects the global scope
+// PURE FUNCTION
 var getVictimInfo = function() {
- victim.name = prompt("Please enter your name:");
- victim.phone = prompt("Please enter your phone number:");
- victim.street = prompt("Please enter your street:");
+	var victim = {};
+	victim.name = prompt("Please enter your name:");
+	victim.phone = prompt("Please enter your phone number:");
+	victim.street = prompt("Please enter your street:");
+	return victim;
 };
 
-getVictimInfo();
+var v1 = getVictimInfo();
+var v2 = getVictimInfo();
+console.log(v1);
 
-alert('Thank you! Victim entered: \n' +
- victim.name + ", " +
- victim.phone + ", " +
- victim.street);
+// want to use this so I can get brand new objects each time the request is made
+// if, for ex, I did console.log v1 and v2 separately, they will give unique values
+
+
+// alert('Thank you! Victim entered: \n' +
+//  victim.name + ", " +
+//  victim.phone + ", " +
+//  victim.street);
 
 // Write a function which removes falsey values from an array. A falsey value is one which evaluates to false when type coerced, which are the following:
 // 0, null, "" (empty string), undefined, NaN, false.
 
-// Write two versions of the function: one that uses side effects and one that is pure.
+// Write two versions of the function: one that uses side effects and one that is pure.ss
